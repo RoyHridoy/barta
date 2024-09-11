c71f55c97f1b094f54579122bbbf37a301a9794d
RoyHridoy <royhridoy1428@gmail.com>
2024-09-11 23:04:24 +0600

    Install Tailwind and setup all views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="text-center p-12 border border-gray-800 rounded-xl">
+        <h1 class="text-3xl justify-center items-center">Welcome to Barta!</h1>
+      </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'index' );
+} );
+
+Route::get( '/login', function () {
+    return view( 'auth.login' );
+} );
+
+Route::get( '/register', function () {
+    return view( 'auth.register' );
+} );
+
+Route::get( '/profile', function () {
+    return view( 'profile.show' );
+} );
+
+Route::get( '/edit-profile', function () {
+    return view( 'profile.edit' );
+} );
