723bf13847645b7a40099298e131c66cddb9223e
RoyHridoy <royhridoy1428@gmail.com>
2025-01-11 00:45:19 +0600

    tests: Comment and Reply Notification tests added

--- tests/Feature/notifications/NewCommentNotificationTest.php
+++ tests/Feature/notifications/NewCommentNotificationTest.php
@@ -4,6 +4,7 @@ use App\Livewire\Comments;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
+use App\Notifications\CommentCreated;
 use Livewire\Livewire;
 
 test('comment notification', function () {
@@ -11,7 +12,12 @@ test('comment notification', function () {
     $commentOwner = User::factory()->create();
     $postOwner = User::factory()->create();
     $post = Post::factory()->create(['user_id' => $postOwner->id]);
-    $comment = Comment::factory()->create(['post_id' => $post->id, 'user_id' => $commentOwner->id]);
+    $comment = Comment::factory()->create([
+        'post_id' => $post->id,
+        'user_id' => $commentOwner->id,
+    ]);
+
+    Notification::fake();
 
     // Act and Assert
     Livewire::actingAs($commentOwner)
@@ -19,9 +25,5 @@ test('comment notification', function () {
         ->set('createCommentForm.body', 'A new comment')
         ->call('createComment');
 
-    $this->assertDatabaseHas('notifications', [
-        'notifiable_id' => $postOwner->id,
-        'notifiable_type' => User::class,
-        'type' => 'comment-created',
-    ]);
+    Notification::assertSentTo($postOwner, CommentCreated::class);
 });
---
+++ tests/Feature/notifications/NewReplyNotificationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Livewire\CommentItem;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use App\Notifications\ReplyCreated;
+use Livewire\Livewire;
+
+test('reply notification', function () {
+    // Arrange
+    $commentOwner = User::factory()->create();
+    $postOwner = User::factory()->create();
+    $replyOwner = User::factory()->create();
+    $post = Post::factory()->create(['user_id' => $postOwner->id]);
+    $comment = Comment::factory()->create([
+        'post_id' => $post->id,
+        'user_id' => $commentOwner->id,
+    ]);
+
+    Notification::fake();
+
+    // Act and Assert
+    Livewire::actingAs($replyOwner)
+        ->test(CommentItem::class, ['comment' => $comment])
+        ->set('createReplyForm.body', 'A new comment')
+        ->call('reply');
+
+    Notification::assertSentTo($commentOwner, ReplyCreated::class);
+});
